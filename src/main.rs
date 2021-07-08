47cdadf16dd352e6c825610776a1d6ae9f307fac
Thai Nguyen <shiroemon279@gmail.com>
2021-07-08 17:47:16 +0700

    Fixing not sending packets issue
    Output like the
    Usage: ping [-t] [-a] [-n count] [-l size] [-f] [-i TTL] [-v TOS]
                [-r count] [-s count] [[-j host-list] | [-k host-list]]
                [-w timeout] [-R] [-S srcaddr] [-c compartment] [-p]
                [-4] [-6] target_name
    
    Options:
        -t             Ping the specified host until stopped.
                       To see statistics and continue - type Control-Break;
                       To stop - type Control-C.
        -a             Resolve addresses to hostnames.
        -n count       Number of echo requests to send.
        -l size        Send buffer size.
        -f             Set Don't Fragment flag in packet (IPv4-only).
        -i TTL         Time To Live.
        -v TOS         Type Of Service (IPv4-only. This setting has been deprecated
                       and has no effect on the type of service field in the IP
                       Header).
        -r count       Record route for count hops (IPv4-only).
        -s count       Timestamp for count hops (IPv4-only).
        -j host-list   Loose source route along host-list (IPv4-only).
        -k host-list   Strict source route along host-list (IPv4-only).
        -w timeout     Timeout in milliseconds to wait for each reply.
        -R             Use routing header to test reverse route also (IPv6-only).
                       Per RFC 5095 the use of this routing header has been
                       deprecated. Some systems may drop echo requests if
                       this header is used.
        -S srcaddr     Source address to use.
        -c compartment Routing compartment identifier.
        -p             Ping a Hyper-V Network Virtualization provider address.
        -4             Force using IPv4.
        -6             Force using IPv6. util

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,68 @@
-use pnet::packet::icmp::{echo_request::MutableEchoRequestPacket, IcmpTypes};
+use pnet::packet::icmp::{
+    echo_reply::EchoReplyPacket, echo_request::MutableEchoRequestPacket, IcmpTypes,
+};
 use pnet::packet::ip::IpNextHeaderProtocols::Icmp;
-use pnet::transport::TransportProtocol::Ipv4;
-use pnet::transport::{icmp_packet_iter, transport_channel, TransportChannelType};
+use pnet::packet::Packet;
+use pnet::transport::{
+    icmp_packet_iter, transport_channel, TransportChannelType::Layer4, TransportProtocol::Ipv4,
+};
+use pnet::util::checksum;
 use std::net::{IpAddr, Ipv4Addr};
+use std::thread::sleep;
+use std::time::{Duration, Instant};
 
 fn main() {
-    let (mut tx, mut rx) = match transport_channel(1500, TransportChannelType::Layer3(Icmp)) {
+    let (mut tx, mut rx) = match transport_channel(1500, Layer4(Ipv4(Icmp))) {
         Ok((tx, rx)) => (tx, rx),
         Err(e) => panic!("An error occurred when creating transport channel: {}", e),
     };
+
     let mut payload = [0u8; 64];
-    let dst = "192.168.100.101".parse::<Ipv4Addr>().unwrap();
+    let dst = IpAddr::V4("192.168.100.101".parse::<Ipv4Addr>().unwrap());
     let mut recv = icmp_packet_iter(&mut rx);
+
+    const IDENT_NUM: u16 = 111;
+    const TTL: u8 = 64;
+    let mut counter: u16 = 1;
+    println!("PING {}: {} data bytes", dst, payload.len());
     loop {
         let mut ping = MutableEchoRequestPacket::new(&mut payload).unwrap();
+
         ping.set_icmp_type(IcmpTypes::EchoRequest);
         ping.set_payload(b"hello");
-        println!("Bytes sent: {}", tx.send_to(ping, IpAddr::V4(dst)).unwrap());
+        ping.set_identifier(IDENT_NUM);
+        ping.set_sequence_number(counter);
+        ping.set_checksum(checksum(ping.packet(), 1));
+
+        let curr_time = Instant::now();
+        tx.set_ttl(TTL).unwrap();
+        tx.send_to(ping, dst).unwrap();
+
         match recv.next() {
-            Ok((pkt, addr)) => println!("{:#?} {}", pkt, addr),
+            Ok((pkt, addr)) => {
+                let pong = EchoReplyPacket::new(pkt.packet()).unwrap();
+                if pong.get_icmp_type() == IcmpTypes::EchoReply
+                    && pong.get_identifier() == IDENT_NUM
+                    && pong.get_sequence_number() == counter
+                {
+                    counter += 1;
+                    let elapsed = curr_time.elapsed();
+                    println!(
+                        "{} bytes from {}: icmp_seq={} ttl={} time={} ms",
+                        payload.len() - 8,
+                        addr,
+                        counter,
+                        TTL,
+                        if elapsed.as_millis() == 0 {
+                            format!("0.{:3}", elapsed.as_micros())
+                        } else {
+                            elapsed.as_millis().to_string()
+                        }
+                    );
+                }
+            }
             Err(e) => eprintln!("Error: {} ", e),
         }
+        sleep(Duration::from_secs(1));
     }
 }
